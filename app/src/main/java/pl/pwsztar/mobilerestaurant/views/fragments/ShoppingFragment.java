c729f866f5fb95790882e5bdadd43ae3ffbfb6ba
Dominik <dominik00801@gmail.com>
2021-06-10 14:27:46 +0200

    Uncommited changes

---
+++ app/src/main/java/pl/pwsztar/mobilerestaurant/views/fragments/ShoppingFragment.java
@@ -0,0 +1,26 @@
+package pl.pwsztar.mobilerestaurant.views.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import pl.pwsztar.mobilerestaurant.R;
+
+public class ShoppingFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_shopping, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    }
+}
